5f08a1b65e36c23f0dca75e1a2f748a7497facfa
daniloldn <112765545+daniloldn@users.noreply.github.com>
2025-11-04 08:42:41 +0000

    paths changed

--- nba/data_load.py
+++ nba/data_load.py
@@ -7,7 +7,7 @@ def load_csv(data_path: Path) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    # TODO: fix this data path using the pathlib library
-    data_path = "/Users/adrianochs/all_my_great_data/game.csv"
-    df = load_csv(data_path)
-    print(data_path)
+    # TODO: fix this data path using the pathlib library    
+    path = Path(__file__).parent/"data"/"game.csv"
+    df = load_csv(path)
+    print(path)
